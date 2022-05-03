3ca772e9dbea5c9a2aee8dbd828a95b0bb815e22
pcMVM <alejandro.perez@mvm.com.co>
2022-05-03 16:04:10 -0500

    Updates

---
+++ precache-manifest.178663b53176e3781ef8f3e43702371c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f23e00a65ec3ff33602283a7f81bb26",
+    "url": "/luisfbcudj94/blockchain-message/index.html"
+  },
+  {
+    "revision": "91c0d8ac0583b3ed4f98",
+    "url": "/luisfbcudj94/blockchain-message/static/css/main.0c1e2795.chunk.css"
+  },
+  {
+    "revision": "b1ef321cfaf5ed71e00d",
+    "url": "/luisfbcudj94/blockchain-message/static/js/2.d4030eec.chunk.js"
+  },
+  {
+    "revision": "91c0d8ac0583b3ed4f98",
+    "url": "/luisfbcudj94/blockchain-message/static/js/main.f8059d3b.chunk.js"
+  },
+  {
+    "revision": "8a5070201c8b2a583139",
+    "url": "/luisfbcudj94/blockchain-message/static/js/runtime-main.f1b31c54.js"
+  }
+]);
